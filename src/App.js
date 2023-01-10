c0b65564aa8b40883489a2c1facd2f5f7cce98cc
profmoriarty1601 <banighosh1601@outlook.com>
2023-01-10 21:27:49 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from 'react-bootstrap';
-import User from './User';
+import { CommonContext } from './components/CommonContext';
 
-function App() {
-
-  const [count, setCount] = useState(0);  
+function App() {  
 
   return (
     <div className="App">
       <h1>React World !!!</h1>
-      <h2>Previous Props with Hooks</h2>
+      <h2>Context API</h2>
       <br />
-      <User count={count} />
-      {/* <h3>Count : {count}</h3> */}
-      <br />
-      <Button onClick={()=>setCount(Math.ceil(Math.random()*10))}> Update Counter </Button>
+      <CommonContext.Provider >
+        <h4>CommonContext</h4>
+      </CommonContext.Provider>
     </div>
   );
 }
